85a9fc83a34b820c7eb0ba383932417093fdbc54
Lucas Soares <lucasoaresleite@gmail.com>
2023-03-16 13:17:11 -0300

    update html change goofy add jquery

--- js/goofy.js
+++ js/goofy.js
@@ -2,6 +2,33 @@
 const loginButton = document.querySelector('.login-btn');
 const login = document.querySelector('.login');
 
-loginButton.addEventListener('click', function() {
-  login.classList.toggle('close');
+
+loginButton.addEventListener('click', function () {
+  let username = document.querySelector('.user').value;
+  let password = document.querySelector('.password').value;
+
+  let uri = "https://sncfinesse1.totvs.com.br/finesse/api/User/" + username
+  saveCredentials(username, password);
+
+    $.ajax({
+    url: uri,
+    credentials:"include",
+    type: 'dataType',
+    /* etc */
+    success: function(jsondata){
+      console.log(jsondata)
+    }
+ })
+
+  // fetch("https://sncfinesse1.totvs.com.br/finesse/api/User/" + username, {
+  //   method: "GET", headers: {'Access-Control-Allow-Origin': '*'}, credentials: "include"
+  // })
+  //   .then(data => console.log(data));
+
+
 });
+
+function
+  saveCredentials(userName, password) {
+  localStorage.setItem('credentials', 'basic ' + userName + ':' + password);
+}
