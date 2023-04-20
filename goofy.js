3d266e262883d35224962a8eee82d008ae3985c5
manzano <matheusmanzano7@gmail.com>
2023-04-20 15:58:29 -0300

    goofy fixes

--- goofy.js
+++ goofy.js
@@ -3,6 +3,7 @@ const login = document.querySelector('.login');
 const selected = document.querySelector('.selected');
 const dropdown = document.querySelectorAll('.dropdown');
 const timer = document.querySelector('#timer');
+const dialer2 = document.querySelector('.dialer');
 
 
 const drop = document.querySelector('.end-icon');
@@ -11,6 +12,8 @@ const contin = document.querySelector('.continue-icon');
 
 let callBtn = document.querySelector(".call");
 let phoneNumber = document.getElementById("cv6");
+let Numberr = document.getElementById("Numberr")
+let FromAddress = document.getElementById("FromAddress")
 let cpf = document.getElementById("cv2");
 let queue = document.getElementById("queue");
 let cti = document.getElementById("cv4");
@@ -27,6 +30,8 @@ const titleQueue = document.getElementById("Tqueue");
 const titleCti = document.getElementById("Tcv4");
 const titleCnpj = document.getElementById("Tcv3");
 const titleTicket = document.getElementById("Tcv8");
+const titleDialedNumber = document.getElementById("Tdn");
+const titleFromAddress = document.getElementById("Tfa");
 
 let u1;
 let p1;
@@ -36,60 +41,64 @@ let statusStartTime;
 let statusCheckInterval;
 let lastUserState;
 let lastUserReasonCodeId;
-
+selected.setAttribute('title', selected.innerText);
 const storedUsername = localStorage.getItem("username");
 const storedPassword = localStorage.getItem("password");
 
 if (storedUsername && storedPassword) {
-  loginUser(storedUsername, storedPassword);
+loginUser(storedUsername, storedPassword);
 }
 
 // Adicione uma nova função para atualizar o status do usuário e o tempo
 function updateStatus(data) {
-  // Obtenha o tempo de início do status atual
-  statusStartTime = new Date(data.getElementsByTagName("stateChangeTime")[0].textContent);
+// Obtenha o tempo de início do status atual
+statusStartTime = new Date(data.getElementsByTagName("stateChangeTime")[0].textContent);
 
-  const userState = data.getElementsByTagName("state")[0].textContent;
-  const userReasonCodeIdElement = data.getElementsByTagName("reasonCodeId")[0];
-  const userReasonCodeId = userReasonCodeIdElement ? userReasonCodeIdElement.textContent : null;
+const userState = data.getElementsByTagName("state")[0].textContent;
+const userReasonCodeIdElement = data.getElementsByTagName("reasonCodeId")[0];
+const userReasonCodeId = userReasonCodeIdElement ? userReasonCodeIdElement.textContent : null;
 
-  if (userState !== lastUserState || userReasonCodeId !== lastUserReasonCodeId) {
-    const menuList = document.querySelector(".menu-list");
-    const options = menuList.querySelectorAll("li");
-    let selectedIndex = -1;
+if (userState !== lastUserState || userReasonCodeId !== lastUserReasonCodeId) {
+	const menuList = document.querySelector(".menu-list");
+	const options = menuList.querySelectorAll("li");
+	let selectedIndex = -1;
 
 	
-    for (let i = 0; i < options.length; i++) {
-      const option = options[i];
-      const id = option.getAttribute("data-id");
+	for (let i = 0; i < options.length; i++) {
+	const option = options[i];
+	const id = option.getAttribute("data-id");
 
-      if (id === userReasonCodeId) {
-        selectedIndex = i;
-        option.classList.add("active");
-        selected.innerText = option.innerText;
-      } else {
-        option.classList.remove("active");
-      }
-    }
+	if (id === userReasonCodeId) {
+		selectedIndex = i;
+		option.classList.add("active");
+		selected.innerText = option.innerText;
+		selected.setAttribute('title', selected.innerText)
+	} else {
+		option.classList.remove("active");
+	}
+	}
 
-    if (userState === "READY") {
-      const readyItem = menuList.querySelector('[data-id="ready"]');
-		
-      if (readyItem) {
-        readyItem.classList.add("active");
-      }
-      selected.innerText = "Ready";
-      document.querySelector('.status-color').style.backgroundColor = '#5AF07B'; // Definir a cor de fundo para READY
-	  incall.style.display = "none"; 
-    }else if(userState === "TALKING"){
+	if (userState === "READY") {
+	const readyItem = menuList.querySelector('[data-id="ready"]');
+
+	if (readyItem) {
+		readyItem.classList.add("active");
+	}
+	selected.innerText = "Ready";
+	selected.setAttribute('title', selected.innerText)
+	document.querySelector('.status-color').style.backgroundColor = '#5AF07B'; // Definir a cor de fundo para READY
+	incall.style.display = "none"; 
+	}else if(userState === "TALKING"){
 		callData(u1,p1);
 		selected.innerText = userState;
+		selected.setAttribute('title', selected.innerText)
 		incall.style.display = "block";
 		document.querySelector('.status-color').style.backgroundColor = '#cea52f';
 
 	}else if(userState === "HOLD"){
 		callData(u1,p1);
 		selected.innerText = userState;
+		selected.setAttribute('title', selected.innerText)
 		incall.style.display = "block";
 		document.querySelector('.status-color').style.backgroundColor = '#cea52f';
 
@@ -97,25 +106,27 @@ function updateStatus(data) {
 		incall.style.display = "none";
 		if(data.getElementsByTagName("label").length > 0){
 			selected.innerText = data.getElementsByTagName("label")[0].textContent;
+			selected.setAttribute('title', selected.innerText)
 		}else{
 			selected.innerText = userState;
-
+			selected.setAttribute('title', selected.innerText)
 		}
 
-      document.querySelector('.status-color').style.backgroundColor = '#BA0707';
+	document.querySelector('.status-color').style.backgroundColor = '#BA0707';
 
-    } else {	
+	} else {	
 		selected.innerText = userState;
+		selected.setAttribute('title', selected.innerText)
 		document.querySelector('.status-color').style.backgroundColor = '#cea52f';
 	}
 
-    // Reset o tempo de início do status quando o agente trocar de status
-    statusStartTime = new Date();
+	// Reset o tempo de início do status quando o agente trocar de status
+	statusStartTime = new Date();
 
-    // Atualize as informações armazenadas
-    lastUserState = userState;
-    lastUserReasonCodeId = userReasonCodeId;
-  }
+	// Atualize as informações armazenadas
+	lastUserState = userState;
+	lastUserReasonCodeId = userReasonCodeId;
+}
 }
 // Atualize a exibição do tempo a cada segundo
 setInterval(() => {
@@ -129,12 +140,38 @@ setInterval(() => {
 	} else {
 		timer.textContent = '0:00';
 	}
-}, 1100);
+}, 1000);
 
 loginButton.addEventListener("click", function() {
 
 });
 
+
+function setLogin(username, password) {
+	let ramal = document.querySelector(".ramal").value;
+	$.ajax({
+		url: "https://sncfinesse1.totvs.com.br/finesse/api/User/"+username,
+		type: "PUT",
+		headers: {
+			"Authorization": "Basic " + btoa(username + ":" + password),
+			"Content-Type": "application/xml"
+		},
+		data: `<User>
+		<state>LOGIN</state>
+		<extension>${ramal}</extension>
+		</User>`,
+		success: function() {
+			console.log("Login succ");
+		},
+		error: function(xhr, status, error) {
+			console.log("Falha login");
+			console.log(ramal);
+			console.log(xhr.status);
+			console.log(xhr.getResponseHeader("Content-Type"));
+		}
+	});
+
+}
 
 function loginUser(username, password) {
 	$.get({
@@ -143,9 +180,14 @@ function loginUser(username, password) {
 			"Authorization": "Basic " + btoa(username + ":" + password),
 		},
 		success: function(data) {
+			let userState = data.getElementsByTagName("state")[0].textContent;
 			u1 = username;
 			p1 = password;
-			ramal = $(data).find('extension').text();			
+			ramal = $(data).find('extension').text();
+			if (userState === "LOGOUT") {
+				setLogin(username, password);		
+			}
+			;
 			updateStatus(data);
 
 			console.log("Request sucess.");
@@ -153,12 +195,13 @@ function loginUser(username, password) {
 
 			localStorage.setItem("username", username);
 			localStorage.setItem("password", password);
+			
 
 			callBtn.addEventListener('click', function(){
 				let dialNumber = document.getElementById("display").value;
 				$.post({
 					url: "https://sncfinesse1.totvs.com.br/finesse/api/User/" + username + "/Dialogs",
-					headers: {
+					headers: {     
 						"Authorization": "Basic " + btoa(username + ":" + password),
 					},
 					dataType: "xml",
@@ -167,7 +210,7 @@ function loginUser(username, password) {
 					<requestedAction>MAKE_CALL</requestedAction>
 					<fromAddress>${ramal}</fromAddress>
 					<toAddress>${dialNumber}</toAddress>
-				 </Dialog>`,
+				</Dialog>`,
 
 					success: function(){
 						console.log("Chamada realizada com sucesso.");
@@ -179,6 +222,8 @@ function loginUser(username, password) {
 					}
 				})
 			});
+
+			
 
 			statusCheckInterval = setInterval(() => {
 				$.get({
@@ -251,6 +296,7 @@ function loginUser(username, password) {
 							select.classList.toggle('select-clicked');
 							caret.classList.toggle('caret-rotate');
 							menu.classList.toggle('menu-open');
+							dialer2.classList.remove('opener');
 						});
 
 						options.forEach(option => {
@@ -330,27 +376,27 @@ function loginUser(username, password) {
 			console.log("DONE");
 		},
 	});
-  }
+}
 // Adicione o evento "click" do loginButton
 loginButton.addEventListener("click", function () {
-  let username = document.querySelector(".user").value;
-  let password = document.querySelector(".password").value;
+let username = document.querySelector(".user").value;
+let password = document.querySelector(".password").value;
 
-  loginUser(username, password);
+loginUser(username, password);
 });
 
 function callData(username,password) {
 	$.get({
-	  url: "https://sncfinesse1.totvs.com.br/finesse/api/User/" + username + "/Dialogs",
-	  headers: {
+	url: "https://sncfinesse1.totvs.com.br/finesse/api/User/" + username + "/Dialogs",
+	headers: {
 		"Authorization": "Basic " + btoa(username + ":" + password),
-	  },
-	  success: function (data) {
+	},
+	success: function (data) {
 	
-	  const callVariable2Value = $(data).find("CallVariable").filter(function() {
-      return $(this).find("name").text() === "callVariable2";
-	  }).find("value").text();
-	  const callVariable3Value = $(data).find("CallVariable").filter(function() {
+	const callVariable2Value = $(data).find("CallVariable").filter(function() {
+	return $(this).find("name").text() === "callVariable2";
+	}).find("value").text();
+	const callVariable3Value = $(data).find("CallVariable").filter(function() {
 		return $(this).find("name").text() === "callVariable3";
 		}).find("value").text();
 		const callVariable4Value = $(data).find("CallVariable").filter(function() {
@@ -364,6 +410,8 @@ function callData(username,password) {
 		}).find("value").text();
 		const queueValue = $(data).find('queueName').text();
 		const callId = $(data).find('uri').text();
+		const dialedNumber = $(data).find('dialedNumber').text();
+		const fromAddress = $(data).find('fromAddress').text();
 
 		checkAndHideIfEmpty(callVariable6Value, phoneNumber, titlePhoneNumber);
 		checkAndHideIfEmpty(callVariable2Value, cpf, titleCpf);
@@ -371,6 +419,8 @@ function callData(username,password) {
 		checkAndHideIfEmpty(callVariable4Value, cti, titleCti);
 		checkAndHideIfEmpty(callVariable8Value, ticket, titleTicket);
 		checkAndHideIfEmpty(queueValue, queue, titleQueue);
+		checkAndHideIfEmpty(dialedNumber, Numberr, titleDialedNumber);
+		checkAndHideIfEmpty(fromAddress, FromAddress, titleFromAddress);
 
 		phoneNumber.textContent = callVariable6Value;		
 		phoneShow.textContent = callVariable6Value;	
@@ -379,6 +429,8 @@ function callData(username,password) {
 		cti.textContent = callVariable4Value;
 		ticket.textContent = callVariable8Value;
 		queue.textContent = queueValue;
+		Numberr.textContent = dialedNumber;
+		FromAddress.textContent = fromAddress;
 
 		drop.addEventListener('click', function (){
 			$.ajax({
@@ -389,7 +441,7 @@ function callData(username,password) {
 					"Content-Type": "application/xml"
 				},
 				data: `<Dialog>
-				<targetMediaAddress>5999</targetMediaAddress>
+				<targetMediaAddress>${ramal}</targetMediaAddress>
 				<requestedAction>DROP</requestedAction>
 				</Dialog>`,
 				success: function() {
@@ -459,7 +511,7 @@ function callData(username,password) {
 				},
 				data: `<Dialog>
 				<targetMediaAddress>${ramal}</targetMediaAddress>
-				<requestedAction></requestedAction>
+				<requestedAction>RETRIEVE</requestedAction>
 				</Dialog>`,
 				success: function() {
 					console.log("Chamada colocada em espera.");
@@ -472,17 +524,17 @@ function callData(username,password) {
 			});
 		});
 		
-	  },
-	  error: function (xhr, status, error) {
+	},
+	error: function (xhr, status, error) {
 		console.log("Failed to get call data.");
 		console.log(xhr.status);
 		console.log(xhr.getResponseHeader("Content-Type"));
 		
-	  },
+	},
 	});
-  }
+}
 
-  function checkAndHideIfEmpty(value, contentElement, titleElement) {
+function checkAndHideIfEmpty(value, contentElement, titleElement) {
 	if (value === "") {
 		contentElement.style.display = "none";
 		titleElement.style.display = "none";
@@ -490,9 +542,5 @@ function callData(username,password) {
 		contentElement.style.display = "";
 		titleElement.style.display = "";
 	}
-  }
-
-
-
-  
+}
 
